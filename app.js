63a45028e54c35e3cd521fe5f877b6392899eb00
Philip Schlosser <phils@Philips-MacBook-Pro.local>
2020-02-29 19:53:07 -0500

    bcrypt

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// encrypting
+//const encrypt = require("mongoose-encryption");
+//hashing
+//var md5 = require("md5");
+// bycrypt
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
 
 //console.log(process.env.ENVY_DORK);
@@ -29,10 +35,11 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
- 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
-const User = new mongoose.model("User",userSchema);
+// encrypting
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const User = new mongoose.model("User", userSchema);
 
 // const  dork = new User({
 //     email:"duck@davis.com",
@@ -41,49 +48,58 @@ const User = new mongoose.model("User",userSchema);
 
 // dork.save();
 
-app.get("/", function(req,res) {
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function(req,res) {
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function(req,res) {
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req,res){
-   const newUser = new User({
-       email:req.body.username,
-       password:req.body.password
-   });
-   newUser.save(function(err){
-       if (err) {
-           console.log(err)
-       } else {
-           res.render("secrets");
-       }
-   });
+app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
 });
 
-app.post("/login", function(req,res){
+
+app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email:username}, function(err,foundUser) {
+    User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
+                //if (foundUser.password === md5(password)) {
+                //    res.render("secrets");
+                //}
             }
         }
     })
 });
 
-app.listen(port, function(){
-    console.log("Listening on port: "+port);
+app.listen(port, function () {
+    console.log("Listening on port: " + port);
 });
